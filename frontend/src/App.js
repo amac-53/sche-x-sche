1ab08f665f7a03fcfd4aba1ff1f47c2e45ad6bc6
YutoFunaki <kaizuka0629@gmail.com>
2023-01-28 16:12:41 +0900

    日本語へ変換

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import jaLocale from '@fullcalendar/core/locales/ja';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-       <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+       <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        locales={[jaLocale]}
+        locale='ja'
+      />
     </div>
   );
 }
