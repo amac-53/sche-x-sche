170f207e2326b33c8ba67708b7b60bff74b0d2d0
YutoFunaki <kaizuka0629@gmail.com>
2023-01-28 16:25:13 +0900

    イベントは追加できるが、ユーザー側で行うことができない...?

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
 import jaLocale from '@fullcalendar/core/locales/ja';
 import './App.css';
 
@@ -8,11 +9,19 @@ function App() {
   return (
     <div className="App">
        <FullCalendar
-        plugins={[dayGridPlugin]}
+        plugins={[dayGridPlugin, timeGridPlugin]}
         initialView="dayGridMonth"
         locales={[jaLocale]}
         locale='ja'
-      />
+        headerToolbar={{
+          left: 'prev,next today', //<>ボタンと「今日」ボタンを配置
+          center: 'title', //「年　月」を配置
+          right: 'dayGridMonth,timeGridWeek', //「月表示」、「週表示」切り替えボタンを配置
+        }}
+        events={[
+          {title:'event', start:'2023-01-20', end:'2023-01-27'}
+        ]}
+       />
     </div>
   );
 }
