da806e4e1a3d38a8c0bd32474f5c9813dbed54d7
amane <amachonchon@gmail.com>
2023-01-29 13:10:53 +0900

    fixed some features

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,55 @@
-import WeekCalendar from 'react-week-calendar/dist/WeekCalendar'; //react-week-calendar読み込み
-import 'react-week-calendar/dist/style.css'; //react-week-calendarのcss読み込み
-import "./App.css"; //そのほかのcss読み込み
-
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import "./App.css";
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import finish_person from './finish_person';
+import { Calendar, momentLocalizer, Views } from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import axois from 'axios'
 
-const events = [
+const localizer = momentLocalizer(moment)
+
+const formats = {
+  dateFormat: 'D',
+  dayFormat: 'D(ddd)',
+  monthHeaderFormat: 'YYYY年M月',
+  dayHeaderFormat: 'M月D日(ddd)',
+  dayRangeHeaderFormat: 'YYYY年M月',
+}
+
+
+const eventList = [
+  {
+    id: 0,
+    title: 'All Day Event very long title',
+    allDay: false,
+    start: new Date('2020-03-01 9:00'),
+    end: new Date('2020-03-01 13:00'),
+  },
   {
     id: 1,
-    title: 'Event 1',
-    start: new Date(2023, 0, 30, 1, 30),
-    end: new Date(2023, 0, 30, 2, 30),
-  },
-  {
-    id: 2,
-    title: 'Event 2',
-    start: new Date(2023, 0, 31, 1, 0),
-    end: new Date(2023, 0, 31, 15, 30),
-  },
+    title: 'Long Event',
+    allDay: false,
+    start: new Date('2023-01-29 15:00'),
+    end: new Date('2023-1-29 17:00'),
+  }
 ];
 
 function App() {
+
+  const [user_name, setUser] = useState('');
+  const [task_name, setTask] = useState('');
+  const [n_team, setNumber] = useState('');
+  const [date, setDate] = useState('');
+  const [start, setStart] = useState('');
+  const [end, setEnd] = useState('');
+
+  // データを送信
+  const sendData = () => {
+    // const data = {}
+    // axios.post("http://localhost:8000/reservations?")
+  }
+  
   return (
     <div className="App">
       <h1>予定合わせる君</h1>
@@ -30,32 +59,54 @@ function App() {
         </Routes>
       </BrowserRouter>
 
-      <div class="name_input">
-        <label for="name">名前<span class="must">*</span>:</label>
-        <input type="text" id="name" name="name" required
-        minlength="4" size="10"></input>
+      <div className="n_team">
+        <label htmlFor="user_name">名前<span className="must">*</span>:</label>
+        <input id="user_name" value={user_name} onChange={(e) => setUser(e.target.value)}
+         required />
       </div>
 
-      <div class="team_name_input">
-        <label for="name">チーム名<span class="must">*</span>:</label>
-        <input type="text" id="name" name="name" required
-        minlength="4" size="10"></input>
+      <div className="n_team">
+        <label htmlFor="task_name">タスク名<span className="must">*</span>:</label>
+        <input id="task_name" value={task_name} onChange={(e) => setTask(e.target.value)} required />
       </div>
 
-      <div class="person_input">
-        <label for="name">チームの人数(数字のみ)<span class="must">*</span>:</label>
-        <input type="text" id="name" name="name" required
-        minlength="4" size="5"></input>
+      <div className="n_team">
+        <label htmlFor="n_team">チームの人数(数字のみ)<span className="must">*</span>:</label>
+        <input type="text" id="n_team" value={n_team} onChange={(e) => setNumber(e.target.value)} required />
       </div>
 
-      <WeekCalendar
-      scaleUnit="30"
-      events = {events}
-      />
+      <div className="person_input">
+        <label htmlFor="date">日付</label>
+        <input type="date" id="date" value={date} onChange={(e) => setDate(e.target.value)} required />
+      </div>
+
+      <div className="person_input">
+        <label htmlFor="start-time">開始時刻</label>
+        <input type="time" id="start-time" value={start} onChange={(e) => setStart(e.target.value)} required />
+      </div>
+
+      <div className="person_input">
+        <label htmlFor="end-time">終了時刻</label>
+        <input type="time" id="end-time" value={end} onChange={(e) => setEnd(e.target.value)} required />
+      </div>
+
+      {
+        <Calendar
+          localizer={localizer}
+          events={eventList}
+          timeslots={2}
+          defaultView={Views.WEEK}
+          onSelectEvent={event => alert(event.title)}
+          views={['month', 'week', 'day']}
+          formats={formats}
+          style={{ height: 500 }}
+        />
+      }
+
       <div>
-        <submit><button>確認</button></submit>
+        <button type="submit" onClick={sendData}>確認</button>
       </div>
-</div>
+    </div>
   );
 }
 
